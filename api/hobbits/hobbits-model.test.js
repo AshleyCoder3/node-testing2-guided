a1fb2fe0cc7f94ed1ec91534df92d7108f437799
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-16 18:24:57 +0200

    wip

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,6 +1,10 @@
 const Hobbit = require('./hobbits-model')
 const db = require('../../data/dbConfig')
 
+beforeAll(async () => {
+  
+})
+
 test('the environment', () => {
   expect(process.env.NODE_ENV).toBe('testing')
 })
