9afa8cdc3a9ca78048fb1d4ff41d7f3a2d440670
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-16 19:30:10 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -25,8 +25,9 @@ server.get("/hobbits/id", (req, res) => {
 });
 
 server.post("/hobbits", async (req, res) => {
-
-  
+  if ()
+  const hobbit = await Hobbits.insert(req.body);
+  res.status(201).json(hobbit)
 });
 
 server.delete("/hobbits/:id", (req, res) => {
