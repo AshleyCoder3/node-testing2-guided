962da876bf8d50bd4a030ff7ea6a902ede691f19
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-16 18:25:44 +0200

    wip

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -2,7 +2,8 @@ const Hobbit = require('./hobbits-model')
 const db = require('../../data/dbConfig')
 
 beforeAll(async () => {
-  
+  await db.migrate.rollback()
+  await db.migrate.latest()
 })
 
 test('the environment', () => {
